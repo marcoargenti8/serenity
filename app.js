577e53518246b77e024458479e5f0bb2558832d8
Marco <marco@Marcos-MacBook-Pro.local>
2017-11-27 20:48:18 +0100

    Redirect all HTTP inbound traffic to HTTPS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var email = require('./controllers/mail.js');
 var app = express();
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
-var http = require('http');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -58,19 +58,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
+	var port = server.address().port;
+	console.log('Magic happens on port ' + port);
 });
 
 //redirect all HTTP inbound traffic to HTTPS
-var httpApp = express();
-var httpRouter = express.Router();
-httpApp.use('*', httpRouter);
-httpRouter.get('*', function(req, res){
-    var host = req.get('Host');
-    host = host.replace(/:\d+$/, ":"+app.get('port'));
-    var destination = ['https://', host, req.url].join('');
-    return res.redirect(destination);
-});
-var httpServer = http.createServer(httpApp);
-httpServer.listen(8080);
+app.get('*',function(req,res,next){
+	if(req.headers['x-forwarded-proto']!='https'){
+		console.log(req.get('Host'));
+		var destination = ['https://', req.get('Host'), req.url].join('');
+		res.redirect(destination);
+	} else {
+		next() /* Continue to other routes if we're not redirecting */
+	}
+})
