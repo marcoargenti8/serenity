f5604570ebd10655206939fcaac0780852de97c8
Marco A <marco.argenti8@gmail.com>
2017-12-03 20:46:57 +0100

    Require confirmation for external url redirect

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 
-$(function(){
+$(function() {
     $('#formEmail').on('submit', function(e) {
         e.preventDefault();
         var data =  $(this).serialize();
@@ -11,15 +11,37 @@ $(function(){
             }
         });
     });
+
+
+
+    // function toggleIcon(e) {
+    //     $(e.target)
+    //         .prev('.panel-heading')
+    //         .find(".more-less")
+    //         .toggleClass('glyphicon-plus-sign glyphicon-minus-sign');
+    // };
+    // $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    // $('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+    $('a').on('click', function(event) {
+      // Get the element and its href attribute.
+        var $element = $(event.target);
+        var link = $element.attr('href');
+        console.log("Redirect to external link" + link);
+
+        if (link.substr(0, 4) == 'http') {
+            // If the link starts with http, assume external link.
+            // In that case, block normal behaviour, insert custom content and navigate after 5 seconds.
+            var continueRedirect = confirm("Stai per lasciare il sito ed essere rediretto verso: \n" + link + "\n\nVuoi continuare?");
+
+            if(continueRedirect){
+                console.log("Redirect to external link" + link);
+                document.location.href = link;
+            } else {
+                event.preventDefault();
+            }
+        }
+    });
 });
 
-
-function toggleIcon(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find(".more-less")
-        .toggleClass('glyphicon-plus-sign glyphicon-minus-sign');
-}
-$('.panel-group').on('hidden.bs.collapse', toggleIcon);
-$('.panel-group').on('shown.bs.collapse', toggleIcon);
 
